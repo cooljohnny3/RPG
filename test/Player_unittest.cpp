6757a218d3de6fdef5873acea0886112841623b7
cooljohnny3 <cooljohnny3@gmail.com>
2018-10-10 23:53:20 -0700

    Begin writing unittests

---
+++ test/Player_unittest.cpp
@@ -0,0 +1,21 @@
+#include "../src/headers/Player.h"
+#include "gtest/gtest.h"
+
+//Player emptyPlayer = Player();
+// TODO look at Player and Creature and fugire out why player has all ints for constructor
+//Player constructedPlayer = Player("Name", );
+
+TEST(PlayerTest, getName) {
+  EXPECT_EQ("", "");
+  //EXPECT_EQ();
+}
+
+TEST(PlayerTest, getMaxHealth) {
+  EXPECT_EQ(100, 100);
+  //EXPECT_EQ();
+}
+
+TEST(PlayerTest, getHealth) {
+  EXPECT_EQ(100, 100);
+  //EXPECT_EQ();
+}
