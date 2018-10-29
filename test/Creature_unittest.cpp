17ea0df16657a8d814a8f8be0941e2abc9983c33
John Brehm <cooljohnny3@gmail.com>
2018-10-29 16:20:25 -0700

    Added health tests for Creature

--- test/Creature_unittest.cpp
+++ test/Creature_unittest.cpp
@@ -14,4 +14,23 @@ TEST(CreatureTest, getHealth) {
   EXPECT_EQ(87, constructedCreature.getHealth());
 }
 
-/* consider using sutomated testing to test sub/add health */
+TEST(CreatureTest, automatedChangeHealth) {
+  Creature creature = Creature("Name", 100, 100);
+  for(int i=0; i > 100; i++) {
+    creature.addHealth(i);
+    creature.subHealth(i);
+    ASSERT_EQ(creature.getHealth(), 100 - i);
+  }
+}
+
+TEST(CreatureTest, subGreaterThanHealth) {
+  Creature creature = Creature("Name", 100, 100);
+  creature.subHealth(101);
+  ASSERT_EQ(creature.getHealth(), creature.getMaxHealth());
+}
+
+TEST(CreatureTest, addGreaterThanHealth) {
+  Creature creature = Creature("Name", 100, 100);
+  creature.addHealth(1);
+  ASSERT_EQ(creature.getHealth(), creature.getMaxHealth());
+}
