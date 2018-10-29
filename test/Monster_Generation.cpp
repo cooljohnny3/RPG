883dbeccd320f25554d1ffb79bb959c750780484
John Brehm <cooljohnny3@gmail.com>
2018-10-29 16:24:12 -0700

    Revert "Added c++11 flag and add more to monster generation test"
    This reverts commit 42130609af110f09b5fe36f1d7c5a345e90c85e2.

--- test/Monster_Generation.cpp
+++ test/Monster_Generation.cpp
@@ -1,17 +1,9 @@
 #include "../src/headers/Monster.h"
 #include "gtest/gtest.h"
 
-bool checkMonster(int level, Monster monster) {
-  int attack = monster.getAttack() - 1;
-  int defence = monster.getDefense();
-  int maxHealth = monster.getMaxHealth() - 10;
-
-  return (attack + defence + maxHealth) == level;
-}
-
-TEST(MonsterGen, automatedGen) {
-  for(int i=0; i < 500; i++) {
+int main() {
+  for(int i=0; i < 100; i++) {
     Monster monster = Monster(i);
-    ASSERT_TRUE(checkMonster(i, monster));
+    std::cout << "Level " << i+1 << ": " << monster.getName() << " " << monster.getMaxHealth() << " " << monster.getAttack() << " " << monster.getDefense() << std::endl;
   }
 }
