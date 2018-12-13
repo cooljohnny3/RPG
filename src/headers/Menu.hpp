92ffbd8ce1864506095b0605bf0ce0cda88d26c0
cooljohnny3 <cooljohnny3@gmail.com>
2018-12-13 11:27:46 -0800

    Added checking for correct choices in menus

--- src/headers/Menu.hpp
+++ src/headers/Menu.hpp
@@ -1,23 +1,34 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
+#include <iostream>
+
 class Menu {
-public:
-	virtual int getChoice() = 0;
+  public:
+    virtual int getChoice() = 0;
+
+  private:
+    virtual bool checkChoice(int) = 0;
 };
 
 class MainMenu : public Menu {
   public:
     int getChoice() {
       int choice;
-      std::cout << "Welcome to RPG!" << std::endl;
-      std::cout << "What would you like to do?" << std::endl;
-      std::cout << "1 - New Game" << std::endl;
-      std::cout << "2 - Load save" << std::endl;
-      std::cout << "3 - Quit" << std::endl;
-      std::cin >> choice;
-      std::cout << std::endl;
+      while(!checkChoice(choice)) {
+        std::cout << "Welcome to RPG!" << std::endl;
+        std::cout << "What would you like to do?" << std::endl;
+        std::cout << "1 - New Game" << std::endl;
+        std::cout << "2 - Load save" << std::endl;
+        std::cout << "3 - Quit" << std::endl;
+        std::cin >> choice;
+        std::cout << std::endl;
+      }
       return choice;
+    }
+  private:
+    bool checkChoice(int choice) {
+      return choice > 0 && choice < 4;
     }
 };
 
@@ -25,15 +36,21 @@ class LoadedMenu : public Menu {
   public:
     int getChoice() {
       int choice;
-      std::cout << "What would you like to do?" << std::endl;
-      std::cout << "1 - Enter dungeon" << std::endl;
-      std::cout << "2 - Rest Up" << std::endl;
-      std::cout << "3 - Status" << std::endl;
-      std::cout << "4 - Save" << std::endl;
-      std::cout << "5 - Quit" << std::endl;
-      std::cin >> choice;
-      std::cout << std::endl;
+      while(!checkChoice(choice)) {
+        std::cout << "What would you like to do?" << std::endl;
+        std::cout << "1 - Enter dungeon" << std::endl;
+        std::cout << "2 - Rest Up" << std::endl;
+        std::cout << "3 - Status" << std::endl;
+        std::cout << "4 - Save" << std::endl;
+        std::cout << "5 - Quit" << std::endl;
+        std::cin >> choice;
+        std::cout << std::endl;
+      }
       return choice;
+    }
+  private:
+    bool checkChoice(int choice) {
+      return choice > 0 && choice < 6;
     }
 };
 
@@ -48,21 +65,31 @@ class TestMenu : public Menu {
     int getChoice() {
       return choice;
     }
+
+  private:
+    bool checkChoice(int choice) {}
 };
 
 class DungeonMenu : public Menu {
   public:
     int getChoice() {
       int choice;
-      // std::cout << "Player: Attack: " << player.getAttack() << " Defence: " << player.getDefense() << " Health: " << player.getHealth() <<std::endl;
-      // std::cout << "Monster: Attack: " << monster.getAttack() << " Defence: " << monster.getDefense() << " Health: " << monster.getHealth() <<std::endl;
-      std::cout << "\nHow do you wish to proceed?" << std::endl;
-      std::cout << "1 - Attack" << std::endl;
-      std::cout << "2 - Use Item" << std::endl;
-      std::cout << "3 - Run Away" << std::endl;
-      std::cin >> choice;
-      std::cout << std::endl;
+      while(!checkChoice(choice)) {
+        // std::cout << "Player: Attack: " << player.getAttack() << " Defence: " << player.getDefense() << " Health: " << player.getHealth() <<std::endl;
+        // std::cout << "Monster: Attack: " << monster.getAttack() << " Defence: " << monster.getDefense() << " Health: " << monster.getHealth() <<std::endl;
+        std::cout << "\nHow do you wish to proceed?" << std::endl;
+        std::cout << "1 - Attack" << std::endl;
+        std::cout << "2 - Use Item" << std::endl;
+        std::cout << "3 - Run Away" << std::endl;
+        std::cin >> choice;
+        std::cout << std::endl;
+      }
       return choice;
+    }
+
+  private:
+    bool checkChoice(int choice) {
+      return choice > 0 && choice < 4;
     }
 };
 
